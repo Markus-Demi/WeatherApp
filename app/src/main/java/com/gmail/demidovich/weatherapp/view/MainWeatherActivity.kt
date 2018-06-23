470fd8f94cd9bc71606aeaf1e769e9d9520f2679
Markus-Demi <m4rk.demi@gmail.com>
2018-06-23 20:30:31 +0300

    Init

---
+++ app/src/main/java/com/gmail/demidovich/weatherapp/view/MainWeatherActivity.kt
@@ -0,0 +1,13 @@
+package com.gmail.demidovich.weatherapp.view
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.gmail.demidovich.weatherapp.R
+
+class MainWeatherActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_weather)
+    }
+}
